398bfe626674bd30d1efa6ba0e4221f3c047cfaa
AidarValshin <Aidar_V2000@mail.ru>
2019-11-24 14:38:15 +0300

    “read+,reset+,model-

--- Workspace_files/behav1.vhd
+++ Workspace_files/behav1.vhd
@@ -49,6 +49,8 @@ ARCHITECTURE am29f400b_behavioral of am29f400b_interface IS
   SIGNAL  BYTE1         :    std_logic := 'U';
   signal t_RC_counter	:    std_logic_vector(3 downto 0);
   signal t_RC_enable	:    std_logic := '0' ; --not allowed counting
+  signal t_RH_counter	:    std_logic_vector(2 downto 0);
+  signal t_RH_enable	:    std_logic := '0' ; --not allowed counting  
 	-----------------------------------------------------------------------------
   TYPE STATE_TYPE IS (
   idle, --waiting state 
@@ -71,80 +73,104 @@ BEGIN
 		S_DOut  <= S_DOut1;
 		NReady  <= NReady1;
 -----------------------------------------------------------------------------
-  state_flow: process (Clk, nRst)
-  begin  -- process state_flow
-  if (nRst= '0') then                   
+state_flow: process (Clk, nRst)
+begin  -- process state_flow
+if (nRst= '0') then                   
     current_state <= reset_s;
-  elsif (rising_edge(clk)) then    
+elsif (rising_edge(clk)) then    
     case current_state is
       
-      when idle =>
+    when idle =>
         if (nCE = '0') and (nWE = '1') then   
           current_state <= read_s;
         elsif (nCE = '0') and (nWE = '0') then
           current_state <= write_s;
         end if;
 		
-      when read_s =>
-        if (t_RC_counter = "0000") then
+    when read_s =>	
+		if (t_RC_counter = "0000") and (t_RC_enable = '0') then
 			current_state <= idle;
-        elsif(t_RC_counter /= "0000") and (nCE /= '0') then
+		elsif(t_RC_counter /= "0000") and (nCE /= '0') then
 			current_state <= reset_s;
+		end if;
+		
+		
+    when write_s =>
+        if (nCE = '1') then
+          current_state <= idle;
+        end if;
+	   
+	when reset_s =>
+        if(t_RH_counter = "000") and (t_RH_enable = '0') then
+          current_state <= idle;
         end if;
 		
-      when write_s =>
-       if (nCE = '1') then
+    when erase =>
+        if (nCE = '1') then
           current_state <= idle;
-       end if;
-	   
-	   when reset_s =>
-       if (nCE = '1') then
-          current_state <= idle;
-       end if;
-		
-      when erase =>
-       if (nCE = '1') then
-          current_state <= idle;
-       end if;
+        end if;
     end case;
   end if;
 end process state_flow;
 -------------------------------------------------------------------------------
- main_flow: process (Clk, nRst)
- begin
-  if (nRst = '0') then
+main_flow: process (Clk, nRst)
+begin
+if (nRst = '0') then
     t_RC_counter <= "1111";
     WE1 <= '0';
     CE1 <= '0';
     OE1 <='0';
-  elsif (Clk'event and Clk = '1') then
+elsif (Clk'event and Clk = '1') then
     if (current_state = read_s  ) then
 		if (t_RC_enable='0') then
 			t_RC_enable <= '1';
 			t_RC_counter <= "1001"; --9 = 90ns
 			A1 <= S_Addr; --save adress in registr
 		elsif(t_RC_enable='1') then
-		if (t_RC_counter /= "0000") then
-			t_RC_counter <= t_RC_counter - '1';
-			DQ1 <= (others=>'Z');
-			WE1 <= '1';
-			CE1 <= '0';
-			OE1 <= '0';
-			S_DOut1 <= DQ1; 
-			NReady1 <=RY ;
-		elsif (t_RC_counter = "0000") then
-			
-			CE1 <= '1';
-			OE1 <= '1';
-			t_RC_enable <= '0';
-		else
-			t_RC_counter <= "1001";
-		end if; 
-    end if;
-  end if;
+			if (t_RC_counter /= "0000") then
+				t_RC_counter <= t_RC_counter - '1';
+				DQ1 <= (others=>'Z');
+				WE1 <= '1';
+				CE1 <= '0';
+				OE1 <= '0';
+				S_DOut1 <= DQ1; 
+				NReady1 <= RY ;
+				RESET1  <= '1';
+			elsif (t_RC_counter = "0000") then				
+				CE1 <= '1';
+				OE1 <= '1';
+				t_RC_enable <= '0';
+			else
+				t_RC_counter <= "1001";
+			end if; 
+		end if;
+	elsif (current_state = reset_s ) then
+		t_RC_enable <= '0'; --not enable counting for counter in read_s state		
+		if (t_RH_enable='0') then
+			t_RH_enable <= '1';
+			t_RH_counter <= "101"; --5 = 50ns
+			RESET1  <= '0';
+		elsif(t_RH_enable='1') then
+			if (t_RH_counter /= "000") then
+				t_RH_counter <= t_RH_counter - '1';
+				CE1 <= '1';
+				OE1 <= '1';
+				NReady1 <= RY ;
+				RESET1  <= '1';
+			elsif (t_RH_counter = "000") then			
+				CE1 <= '0';
+				OE1 <= '0';
+				t_RH_enable <= '0';
+			else
+				t_RH_counter <= "101";
+			end if; 
+		
+		end if;
+	end if;
 end if;
 end process main_flow;
 END am29f400b_behavioral;
+
 
 
 
